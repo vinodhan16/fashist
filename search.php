8b82a0f2bf581e0a6a3b5831bf085cbc18116677
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2019-03-28 09:47:37 +0530

    Update search.php

--- search.php
+++ search.php
@@ -15,70 +15,6 @@
 
 </head>
 <body>
-<<<<<<< HEAD
-<div class="container top">
-    <div class="card card-signin my-5">
-          <div class="card-body" style="background-color:#ffffff;">
-              <div class="container1">
-                   <h2 class="card-title text-center">Search Outfit</h2>
-                  <br>
-                  <p class="measure">Occassions</p>
-     <form class="form-signin" method="post" action="">
-         <div class="row">
-             <div class="col-md-2">
-             <div class="form-label-group" >
-                  <div class="dropdown" >
-    <button  class="btn btn-default dropdown-toggle"  type="button" style="width:100%;" data-toggle="dropdown">Search Products
-    <span class="caret"></span></button>
-    <ul class="dropdown-menu" style="border-radius:13px;">
-         <li class="dropdown-submenu">
-        <a class="test" tabindex="-1" href="#">Office <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a tabindex="-1" value="western" onclik>Western</a></li>
-          <li><a tabindex="-1" href="#">Indian</a></li>
-        </ul>
-      </li>
-      <li><a tabindex="-1" href="#">Sports</a></li>
-         <li class="dropdown-submenu">
-        <a class="test" tabindex="-1" href="#">Marriage <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a tabindex="-1" href="#">Western</a></li>
-          <li><a tabindex="-1" href="#">Indian</a></li>
-        </ul>
-      </li>
-      <li><a tabindex="-1" href="#">Casuals</a></li>
-        <li><a tabindex="-1" href="#">Indian Festivals</a></li>
-     
-    </ul>
-  </div>
-              </div></div>
-             <div class="col-md-6">  <input name="bust" type="text" class="form-control" style="border-radius:15px;padding:10px;" placeholder="Search Clothing for Occassions..." value="" required/></div>
-             <div class="col-md-3">
-                 <button name="login" type="submit" style="background-color:#f229cd;color:#ffffff;" class="btn  btn-default  text-uppercase" type="submit">Search</button></div>
-           </div> </form></div>
-        </div></div></div>
-    <script>
-$(document).ready(function(){
-  $('.dropdown-submenu a.test').on("click", function(e){
-    $(this).next('ul').toggle();
-    e.stopPropagation();
-    e.preventDefault();
-  });
-});
-</script>
-    <style>
-        .btn-default:hover{
-            text-decoration: none !important;
-          
-        }
-        .dropdown
-        {
-            text-align: left !important;
-        }
-        .dropdown-submenu {
-  position: relative;
-}
-=======
   <div class="container">
     <div class="row">
         <div class="col-sm-12 col-md-5 col-lg-6 mx-auto">
@@ -88,7 +24,6 @@ $(document).ready(function(){
       <div class="col-sm-12 col-md-7 col-lg-6 mx-auto">
         <div class="card card-signin my-5">
           <div class="card-body">
->>>>>>> parent of bc8d304... ssss
 
                <div class="tab-content" id="myTabContent">
                      <div class="tab-pane fade show active p-3" id="one" role="tabpanel" aria-labelledby="one-tab">
