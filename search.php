be471a9bb58add2025c65abdeda0f3456f6a866c
saicharan96 <saicharan14996@gmail.com>
2019-03-28 15:33:02 +0530

    sssss
    ssss

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <meta charset="utf-8">
+ <meta charset="utf-8">
   <title>Fashist</title>
   <link rel="icon" href="./assets/images/logo.png" type="image" sizes="16x16">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,38 +8,48 @@
   <link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans|Roboto:500" rel="stylesheet">
   <link rel="stylesheet" href="./assets/css/style.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Aladin|Bad+Script|Handlee|Kaushan+Script|Rancho" rel="stylesheet">
-
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-  <div class="">
+  <div class="products container1">
     <div class="row">
-        <div class="col-xs-12 col-md-2  mx-auto">
-            <br>
-       <img src="assets/images/demo.jpg" style="max-width:100px;width:100%;">
+      <div class="col-md-2">
+        <center><img class="demo" src="assets/images/demo.jpg"></center>
         </div>
-      <div class="col-xs-12 col-md-10  mx-auto">
-        <div class="card card-signin ">
-          <div class="card-body" >
-            <form class="form-signin" method="post" action="result.php">
-                     <div class="row">
-                 <div class="col-xs-8 col-md-10 col-lg-10 mx-auto">
-              <div class="form-label-group">
-                <input name="search" type="text" class="form-control" placeholder="Occasion Type" required autofocus>
-              </div></div>
-                  <div class="col-xs-4 col-md-2 col-lg-2 mx-auto">
-                      <button  type="submit" style="background-color:#f229cd;color:#ffffff;" class="btn btn-lg btn-default btn-block text-uppercase" type="submit">Find Now!</button></div>
-                <br>
-</div>
-                  </form>
-                  
+         <div class="col-md-10 product">
+             <div class="container1">
+             <div class="row">
+                 <div class="col-xs-6">
+         <div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Select by category 
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+         <li class="dropdown-submenu">
+        <a class="test" tabindex="-1" href="#">Office <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a tabindex="-1" href="#">Western</a></li>
+          <li><a tabindex="-1" href="#">Indian</a></li>
+        </ul>
+      </li>
+      <li><a tabindex="-1" href="#">Sports</a></li>
+         <li class="dropdown-submenu">
+        <a class="test" tabindex="-1" href="#">Marriage <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a tabindex="-1" href="#">Bridesmaid</a></li>
+          <li><a tabindex="-1" href="#">Bride</a></li>
+        </ul>
+      </li>
+      <li><a tabindex="-1" href="#">Casuals</a></li>
+         <li><a tabindex="-1" href="#">Indian Festivals</a></li>
+     
+    </ul>
+  </div></div>
+                 <div class="col-xs-6">  <center> <button name="search" type="submit" style="background-color:#f229cd;color:#ffffff;" class="btn  btn-default radius  text-uppercase" type="submit">Search Now !</button></center> </div>
+                 </div>
         </div>
-           <div class="container">
-                <p class="measure">Search Results</p>
-               <br>
+     <div class="top">
             <div class="row">
                 <div class="col-md-3 col-sm-6 col-xs-6">
                     <div class="product-grid2">
@@ -50,89 +60,104 @@
                         </div>
                         <div class="product-content">
                             <h3 class="title"><a href="#">Women's Designer Top</a></h3>
-                            <p  class="title">Available on:</p>
-                            <div class="container">
-                            <div class="row">
-                            <div class="col-xs-4"><div class="navy"><img class="size" src="assets/images/amazon.png"></div></div>
-                                <div class="col-xs-4"><div class="blue"><img class="size" src="assets/images/flipkart.png"></div></div>
-                                <div class="col-xs-4"><div class="red"><img class="size" src="assets/images/snap.png"></div></div>
-                            </div></div>
-                            <br>
+                            <div class="row cont">
+                            <div class="col-xs-4 nopadding">
+                                <div class="navy"><img class="mini" src="assets/images/amazon.png"></div>
+                            </div>
+                                <div class="col-xs-4 nopadding">
+                                 <div class="blue"><img class="mini" src="assets/images/flipkart.png"></div>
+                            </div>
+                                <div class="col-xs-4 nopadding">
+                                 <div class="red"><img class="mini" src="assets/images/snap.png"></div>
+                            </div>
+                            </div>
                             <span class="price">₹ 599.99</span>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 col-sm-6 col-xs-6">
+               <div class="col-md-3 col-sm-6 col-xs-6">
                     <div class="product-grid2">
                         <div class="product-image2">
                             <a href="#">
-                                <img class="pic-1" src="./assets/images/img2.jpg">
+                                <img class="pic-1" src="./assets/images/img1.jpg">
                             </a>
-                        
                         </div>
                         <div class="product-content">
                             <h3 class="title"><a href="#">Women's Designer Top</a></h3>
-                               <p  class="title">Available on:</p>
-                                       <div class="container">
-                            <div class="row">
-                            <div class="col-xs-4"><div class="navy"><img class="size" src="assets/images/amazon.png"></div></div>
-                                <div class="col-xs-4"><div class="blue"><img class="size" src="assets/images/flipkart.png"></div></div>
-                                <div class="col-xs-4"><div class="red"><img class="size" src="assets/images/snap.png"></div></div>
-                            </div></div>
-                            <br>
+                            <div class="row cont">
+                            <div class="col-xs-4 nopadding">
+                                <div class="navy"><img class="mini" src="assets/images/amazon.png"></div>
+                            </div>
+                                <div class="col-xs-4 nopadding">
+                                 <div class="blue"><img class="mini" src="assets/images/flipkart.png"></div>
+                            </div>
+                                <div class="col-xs-4 nopadding">
+                                 <div class="red"><img class="mini" src="assets/images/snap.png"></div>
+                            </div>
+                            </div>
                             <span class="price">₹ 599.99</span>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 col-sm-6 col-xs-6">
+               <div class="col-md-3 col-sm-6 col-xs-6">
                     <div class="product-grid2">
                         <div class="product-image2">
                             <a href="#">
-                                <img class="pic-1" src="./assets/images/img3.jpg">
+                                <img class="pic-1" src="./assets/images/img1.jpg">
                             </a>
-                         
                         </div>
                         <div class="product-content">
                             <h3 class="title"><a href="#">Women's Designer Top</a></h3>
-                           <p  class="title">Available on:</p>
-                                       <div class="container">
-                            <div class="row">
-                            <div class="col-xs-4"><div class="navy"><img class="size" src="assets/images/amazon.png"></div></div>
-                                <div class="col-xs-4"><div class="blue"><img class="size" src="assets/images/flipkart.png"></div></div>
-                                <div class="col-xs-4"><div class="red"><img class="size" src="assets/images/snap.png"></div></div>
-                            </div></div>
-                            <br>
+                            <div class="row cont">
+                            <div class="col-xs-4 nopadding">
+                                <div class="navy"><img class="mini" src="assets/images/amazon.png"></div>
+                            </div>
+                                <div class="col-xs-4 nopadding">
+                                 <div class="blue"><img class="mini" src="assets/images/flipkart.png"></div>
+                            </div>
+                                <div class="col-xs-4 nopadding">
+                                 <div class="red"><img class="mini" src="assets/images/snap.png"></div>
+                            </div>
+                            </div>
                             <span class="price">₹ 599.99</span>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 col-sm-6 col-xs-6">
+                 <div class="col-md-3 col-sm-6 col-xs-6">
                     <div class="product-grid2">
                         <div class="product-image2">
                             <a href="#">
-                                <img class="pic-1" src="./assets/images/img4.jpg">
+                                <img class="pic-1" src="./assets/images/img1.jpg">
                             </a>
-                    
                         </div>
                         <div class="product-content">
                             <h3 class="title"><a href="#">Women's Designer Top</a></h3>
-                          <p  class="title">Available on:</p>
-                             <div class="container">
-                            <div class="row">
-                            <div class="col-xs-4"><div class="navy"><img class="size" src="assets/images/amazon.png"></div></div>
-                                <div class="col-xs-4"><div class="blue"><img class="size" src="assets/images/flipkart.png"></div></div>
-                                <div class="col-xs-4"><div class="red"><img class="size" src="assets/images/snap.png"></div></div>
-                            </div></div>
-                            <br>
+                            <div class="row cont">
+                            <div class="col-xs-4 nopadding">
+                                <div class="navy"><img class="mini" src="assets/images/amazon.png"></div>
+                            </div>
+                                <div class="col-xs-4 nopadding">
+                                 <div class="blue"><img class="mini" src="assets/images/flipkart.png"></div>
+                            </div>
+                                <div class="col-xs-4 nopadding">
+                                 <div class="red"><img class="mini" src="assets/images/snap.png"></div>
+                            </div>
+                            </div>
                             <span class="price">₹ 599.99</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        
-    </div>
-        </div></div></div>
+</div>
+      </div></div>
+    <script>
+$(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
+</script>
 </body>
-</html>
