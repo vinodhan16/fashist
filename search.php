d82a6413c46e270b3501960334800e203024e436
saicharan96 <saicharan14996@gmail.com>
2019-03-30 07:37:06 +0530

    ssss
    sssss

--- search.php
+++ search.php
@@ -16,35 +16,27 @@
   <div class="products container1">
     <div class="row">
       <div class="col-md-2">
-        <center><img class="demo" src="assets/images/demo.png"></center>
+       <img class="demo" src="assets/images/demo.png">
         </div>
          <div class="col-md-10 product">
              <div class="container1">
              <div class="row">
                  <div class="col-xs-6">
          <div class="dropdown">
-    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Select by category
-    <span class="caret"></span></button>
-    <ul class="dropdown-menu">
-         <li class="dropdown-submenu">
-        <a class="test" tabindex="-1" href="#">Office <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a tabindex="-1" href="#">Western</a></li>
-          <li><a tabindex="-1" href="#">Indian</a></li>
-        </ul>
-      </li>
-      <li><a tabindex="-1" href="#">Sports</a></li>
-         <li class="dropdown-submenu">
-        <a class="test" tabindex="-1" href="#">Marriage <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a tabindex="-1" href="#">Bridesmaid</a></li>
-          <li><a tabindex="-1" href="#">Bride</a></li>
-        </ul>
-      </li>
-      <li><a tabindex="-1" href="#">Casuals</a></li>
-         <li><a tabindex="-1" href="#">Indian Festivals</a></li>
-
-    </ul>
+  <select class="form-control" style="min-height:40px;border-radius:16px;font-size:20px;font-weight:400px;">
+  <optgroup label="Office">
+    <option value="volvo"  type="square">* Western</option>
+    <option value="saab">* Indian</option>
+  </optgroup>
+         <option value="saab">Sports</option>
+  <optgroup label="Marriage">
+    <option value="mercedes">*Bridesmaid</option>
+    <option value="audi">*Bride</option>
+  </optgroup>
+      <option value="saab">Casuals</option>
+      <option value="saab">Indian Festivals</option>
+</select>
+  
   </div></div>
                  <div class="col-xs-6">  <center> <button name="search" type="submit" style="background-color:#f229cd;color:#ffffff;" class="btn  btn-default radius  text-uppercase" type="submit">Search Now !</button></center> </div>
                  </div>
